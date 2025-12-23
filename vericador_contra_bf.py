23487a45bb3f63cb49736a67da6c0870bb872cb8
petrakiio <eup0022@gmail.com>
2025-12-23 01:09:54 -0300

    Fazer melhoria em tratamento de maiuscula e minuscula

--- vericador_contra_bf.py
+++ vericador_contra_bf.py
@@ -24,14 +24,54 @@ def analisar(senha,numeros,alfabeto,simbolos):
     
     else:
         return "COMPOSTO"
- 
-tentativas = 0
+def testar(digito,tipo,opn,senha):
+    numeros = "1234567890"
+    alfabeto = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz"
+    simbolos = "!@#$%^&*()-_=+[{]};:,.<>?/|"
+    tentativa = 0 
+
+    if tipo == "MISTA":
+        dado = numeros+alfabeto+simbolos
+    elif tipo == "SIMBOLOS":
+        dado = simbolos
+    elif tipo == "LETRAS":
+        dado = alfabeto
+    elif tipo == "NUM":
+        dado = numeros
+    else:
+        dado = numeros+alfabeto+simbolos
+    
+    if opn == True:
+        while True:
+            combinação = "".join(random.choices(dado,k=digito))
+            tentativa +=1
+            print(f"Senha testada:{combinação}.Tentativa:{tentativa}")
+            if combinação == senha:
+                print("Sua senha foi quebrada pelo computador!")
+                if tentativa >= 10000000000:
+                    print("Paramos os testes por segurança!")
+                    break
+                else:
+                    pass
+            else:
+                pass
+    else:
+        while True:
+            combinação = random.choices(dado,k=digito)
+            if combinação == senha:
+                print("Sua senha foi quebrada pelo computador!")
+            else:
+                pass
+  
+        
 numeros = "1234567890"
 alfabeto = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz"
-simbolos = "!@#$%^&*()-_=+[{]};:,.<>?/|"
-dados = numeros+alfabeto+simbolos
+simbolos = "!@#$%^&*()-_=+[{]};:,.<>?/|" 
+tentativas = 0
 
 senha = input("Me diga sua senha forte:")
+tipo_de_senha = analisar(senha,numeros,alfabeto,simbolos)
+digitos = contar_caracteres(senha)
 print("O progama vai tentar quebra-lá")
 opn = int(input("Você quer ver o programa testando as senhas?\n1-Sim/2-Não:"))
 if opn == 1:
@@ -43,7 +83,8 @@ with open("password-wordlist.txt") as wordlist:
     for senhas in wordlist:
         senhas = senhas.strip()
         if ver_senhas == True:
-            print(f"Senha testada:{senhas},Tentiva:{tentativas +1}")
+            tentativas +=1 
+            print(f"Senha testada:{senhas},Tentiva:{tentativas}")
             if senhas == senha:
                 print("Sua senha foi achada na wordlist!")
                 break
@@ -53,24 +94,10 @@ with open("password-wordlist.txt") as wordlist:
                 break
     else:
         opn2 = int(input("Sua senha não foi encontrada na wordlist,você deseja tentar um ataque de força bruta nela?\n 1-Sim/2-Não:"))
-        if opn2 == 1 and ver_senhas == True:
-            while True:
-                digitos = contar_caracteres(senha)
-                combinação = "".join(random.choices(dados, k=digitos))
-                print(f"Senha testada:{combinação},Tentiva:{tentativas +1}")
-                if combinação == senha:
-                    print("Sua senha foi quebrada!")
-                    break
-                else:
-                    pass
-        elif opn2 == 1 and ver_senhas == False:
-            while True:
-                digitos = contar_caracteres(senha)
-                combinação = "".join(random.choices(dados, k=digitos))
-                if combinação == senha:
-                    print("Sua senha foi querada!")
-                    break
-                else:
-                    pass
+        if opn2 == 1:
+            print(*"=")
+            print("Vamos testar sua senha!")
+            print(*"=")
+            testar(digitos,tipo_de_senha,ver_senhas,senha)
         else:
-            print("Tudo bem,sua senha é forte!")
+            print("tudo bem,sua senha é forte!")
