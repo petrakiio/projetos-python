1a2c8a925582dcd25e67a28922f0d5c1af021077
petrakiio <eup0022@gmail.com>
2026-02-23 19:35:48 -0500

    Fazendo um bot multi funções

--- bot/connection.py
+++ bot/connection.py
@@ -1,2 +1,19 @@
+import requests
 
-
+def Buscar_CEP(cep):
+    url = f"https://viacep.com.br/ws/{cep}/json/"
+    reposta = requests.get(url)
+    dados = reposta.json()
+    if "erro" in dados:
+        return "Erro no CEP"
+    else:
+        cidade = {
+            "Rua":dados['logradouro'],
+            "Bairro":dados["bairro"],
+            "Cidade":dados["localidade"],
+            "Estado":dados["uf"]
+        }
+        return cidade
+    
+def Buscar_ip(ip):
+    url = ""
