7ebfd1761846f06f9738f46a72959f06f288c427
petrakiio <eup0022@gmail.com>
2026-04-11 19:29:06 -0500

    abrindo pastas

--- getarquives.py
+++ getarquives.py
@@ -1,3 +1,21 @@
 """
 Get Arquives with python
 """
+
+#Import libs
+import os
+
+home = os.path.expanduser("~")
+pastas = ['Documentos','Imagens','Downloads','Vídeos','Músicas','Modelos']
+
+def search():
+    for p in pastas:
+        caminho = os.path.join(home, p)
+        if os.path.isdir(caminho):
+            arquivos = os.listdir(caminho)
+            print(f"\n== {caminho} ==")
+            print(arquivos)
+        else:
+            print(f"\nPasta não encontrada: {caminho}")
+
+search()
