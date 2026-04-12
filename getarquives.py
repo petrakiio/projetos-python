08d4c304a446a6c92dd5a5b9892ec7b67f3bcf18
petrakiio <eup0022@gmail.com>
2026-04-11 19:32:27 -0500

    Busca feita

--- getarquives.py
+++ getarquives.py
@@ -8,14 +8,16 @@ import os
 home = os.path.expanduser("~")
 pastas = ['Documentos','Imagens','Downloads','Vídeos','Músicas','Modelos']
 
-def search():
+def search(arq):
     for p in pastas:
         caminho = os.path.join(home, p)
         if os.path.isdir(caminho):
             arquivos = os.listdir(caminho)
-            print(f"\n== {caminho} ==")
-            print(arquivos)
+            if arq in arquivos:
+                print(f'Arquivo encontrado em:[{caminho}/{arq}]')
+                break
         else:
             print(f"\nPasta não encontrada: {caminho}")
 
-search()
+
+search('fundo.jpg')
